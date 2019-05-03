17ee50769a08506e7ea0a41218a4c2d30f934e1e
ak <annak2@illinois.edu>
2019-05-03 03:06:49 -0500

    Added ImageURL field to db models

--- models/apartment.js
+++ models/apartment.js
@@ -9,7 +9,8 @@ var ApartmentSchema = new mongoose.Schema({
     EndDate: {type: Date, required: true},
     Bedrooms: {type: Number, default: undefined},
     Bathrooms: {type: Number, default: undefined},
-    userID: {type: String, required: true}
+    UserID: {type: String, required: true},
+   	ImageURL: {type: String, default: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjt5JXZz_7hAhUOUK0KHaW8BmkQjRx6BAgBEAU&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fcbdamericanshaman.com%252Fwater-soluble-full-spectrum-hemp-oil-30ml%26psig%3DAOvVaw37AF11wkHY4FckbGVORkg_%26ust%3D1556947501828534&psig=AOvVaw37AF11wkHY4FckbGVORkg_&ust=1556947501828534'}
 });
 
 // Export the Mongoose model
--- models/user.js
+++ models/user.js
@@ -4,11 +4,12 @@ var mongoose = require('mongoose');
 // Define our user schema
 var UserSchema = new mongoose.Schema({
     _id: {type: String, auto: true, required: true}, 
-    currentApartments: {type: Array},
-    savedApartments: {type: Array},
-    cellPhone: {type: String, required: true},
-    email: {type: String, required: true},
-    name: {type: String, required: true}
+    CurrentApartments: {type: Array},
+    SavedApartments: {type: Array},
+    CellPhone: {type: String, required: true},
+    Email: {type: String, required: true},
+    Name: {type: String, required: true},
+    ImageURL: {type: String, default: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjt5JXZz_7hAhUOUK0KHaW8BmkQjRx6BAgBEAU&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fcbdamericanshaman.com%252Fwater-soluble-full-spectrum-hemp-oil-30ml%26psig%3DAOvVaw37AF11wkHY4FckbGVORkg_%26ust%3D1556947501828534&psig=AOvVaw37AF11wkHY4FckbGVORkg_&ust=1556947501828534'}
 });
 
 // Export the Mongoose model
